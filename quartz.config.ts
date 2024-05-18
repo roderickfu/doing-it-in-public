c7fcf9bdfe7f1fe26c6b01401194077919853e6c
haohsuanfu <roderickfu@gmail.com>
2024-05-18 13:35:04 +0800

    Quartz sync: May 18, 2024, 1:35 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -32,7 +32,7 @@ const config: QuartzConfig = {
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
-          dark: "#a60303", //#2b2b2b
+          dark: "#2b2b2b", //#2b2b2b
           secondary: "#284b63",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
@@ -42,7 +42,7 @@ const config: QuartzConfig = {
           lightgray: "#393639",
           gray: "#646464",
           darkgray: "#d4d4d4",
-          dark: "#a60303", //#ebebec
+          dark: "#ebebec", //#ebebec
           secondary: "#7b97aa",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
